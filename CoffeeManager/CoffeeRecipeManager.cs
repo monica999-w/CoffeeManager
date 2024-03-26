d7e0fefee0cd572b10f9d59f3fbaf8a5a7b5a516
Monica Iliescu <monica.iliescu@amdaris.com>
2024-03-26 14:11:46 +0200

    Add project files.

---
+++ CoffeeManager/CoffeeRecipeManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeManager
+{
+        public class CoffeeRecipeManager
+        {
+            private static CoffeeRecipeManager? instance;
+            public Dictionary<string, string> recipes;
+
+            public CoffeeRecipeManager()
+            {
+                recipes = new Dictionary<string, string>();
+                InitializeRecipe();
+            }
+
+            public static CoffeeRecipeManager Instance
+            {
+                get
+                {
+                    if (instance == null)
+                    {
+
+                        instance = new CoffeeRecipeManager();
+
+                    }
+                    return instance;
+
+                }
+            }
+
+            public void InitializeRecipe()
+            {
+                recipes["Espresso"] = "Black coffee";
+                recipes["Cappuccino"] = "Black coffee + Milk";
+                recipes["Flat White"] = "Black coffee + Black coffee + Milk";
+            }
+            public void AddCustemerRecipe(string name, string ingrediants)
+            {
+                if (!recipes.ContainsKey(name))
+                {
+                    recipes[name] = ingrediants;
+                    Console.WriteLine($"Custermer recipe:  {name}  - add successful \n");
+
+                }
+                else
+                {
+                    Console.WriteLine($"Recipe :  {name} -  exist \n");
+                }
+            }
+
+            public void DisplayRecipes()
+            {
+                Console.WriteLine(" Recipes : \n");
+
+                foreach (var recipe in recipes)
+                {
+                    Console.WriteLine($"{recipe.Key} : {recipe.Value}");
+                }
+
+            }
+
+
+        }
+    
+}
---
+++ CoffeeManager/Program.cs
@@ -0,0 +1,18 @@
+﻿using CoffeeManager;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CoffeeRecipeManager recipeManager = new CoffeeRecipeManager();
+        //recipeManager.DisplayRecipes();
+
+        recipeManager.AddCustemerRecipe("Regular Milk", "Espresso + Soy milk + Sugar");
+        recipeManager.AddCustemerRecipe("Oat Milk", "Cappuccino + Sugar+ Sugar");
+        recipeManager.AddCustemerRecipe("Soy Milk", "Flat white + Sugar");
+
+        recipeManager.AddCustemerRecipe("Regular Milk", "Cappuccino + Sugar+ Sugar");
+
+        recipeManager.DisplayRecipes();
+    }
+}
